3eb4e68b74694b86d1a767a69d14edccd89663cd
Timo Sandmann <mail@timosandmann.de>
2021-05-02 14:06:22 +0200

    gcc 11.1 for m0+, m4f, m7f

--- arm-cortexm0p-eabi/lib/libstdc++.a-gdb.py
+++ arm-cortexm0p-eabi/lib/libstdc++.a-gdb.py
@@ -1,5 +1,5 @@
 # -*- python -*-
-# Copyright (C) 2009-2020 Free Software Foundation, Inc.
+# Copyright (C) 2009-2021 Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/usr/local/arm-none-eabi/share/gcc-10.3.0/python'
+pythondir = '/usr/local/arm-none-eabi/share/gcc-11.1.0/python'
 libdir = '/usr/local/arm-none-eabi/arm-cortexm0p-eabi/lib'
 
 # This file might be loaded when there is no current objfile.  This
